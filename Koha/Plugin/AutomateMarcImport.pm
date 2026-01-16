a5fb87ab2f223cb5a8b6cadc7fd6189556387de8
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-01-16 14:52:21 +0000

    chore: bump version

--- Koha/Plugin/AutomateMarcImport.pm
+++ Koha/Plugin/AutomateMarcImport.pm
@@ -24,7 +24,7 @@ use Koha::ImportBatchProfiles;
 use Koha::Logger;
 use Koha::UploadedFile;
 
-our $VERSION = '1.0.6';
+our $VERSION = '1.0.7';
 
 our $metadata = {
     name            => 'Automate Marc Import',
