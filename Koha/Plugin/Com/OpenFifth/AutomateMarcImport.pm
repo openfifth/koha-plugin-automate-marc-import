4efad6458098366f0e80ead08b4946ea6e4adee2
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-01-20 08:00:34 +0000

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/AutomateMarcImport.pm
+++ Koha/Plugin/Com/OpenFifth/AutomateMarcImport.pm
@@ -26,13 +26,13 @@ use Koha::ImportBatchProfiles;
 use Koha::Logger;
 use Koha::UploadedFile;
 
-our $VERSION = '1.0.16';
+our $VERSION = '1.0.17';
 
 our $metadata = {
     name            => 'Automate Marc Import',
     author          => 'Open Fifth',
     date_authored   => '2022-05-19',
-    date_updated    => '2026-01-19',
+    date_updated    => '2026-01-20',
     minimum_version => '24.11.00.000', #TODO: update this to relevant Koha version once knows (dependency not yet upstreamed)
     maximum_version => undef,
     version         => $VERSION,
